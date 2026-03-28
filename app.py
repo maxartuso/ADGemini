253a4c36b14df18738e4bafad958d4b27258dc87
maxartuso <163533735+maxartuso@users.noreply.github.com>
2026-03-28 17:11:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if query:
         # Application des instructions au modèle
         # 2. Appel au modèle Gemini (Flash 1.5 ou 2.0)
         model_gemini = (genai.GenerativeModel('gemini-flash-latest'),
-            system_instruction==INSTRUCTIONS_CONSEILLER
+            system_instruction=INSTRUCTIONS_CONSEILLER
         )      
 
         prompt = f"""Tu es l'expert d'Autisme Diffusion (AFD). 
